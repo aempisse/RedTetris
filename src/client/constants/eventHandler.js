cc3bc0d737900f47ce5d3137bd176aa563da1688
Maxime LORIN <malorin@e1r5p13.42.fr>
2018-08-05 19:56:40 +0200

    ajout eventHandler

--- src/client/constants/eventHandler.js
+++ src/client/constants/eventHandler.js
@@ -1,20 +1,31 @@
 import {updateTetri} from "../actions/update_tetri.js";
-import {store} from "../store/index.js";
+import {moveRight} from "../actions/moveRight.js";
+import {moveLeft} from "../actions/moveLeft.js";
+import {moveRot} from "../actions/moveRot.js";
+import store from "../store/index";
 
-const keyEnter = 13;
-const keySpace = 32;
-const keyLeft = 37;
-const keyUp = 38;
-const keyRight = 39;
-const keyDown = 40;
-const keyS = 83;
-const keyC = 67;
-
-const eventHandler = (event) => {
-    switch (event.keyCode) {
+const keyLeft = 65;
+const keyUp = 87;
+const keyRight = 68;
+const keyDown = 83;
+  
+export const eventHandler = (event) => {
+     switch (event.keyCode) {
         case keyDown:
-        event.preventDefault();
-        store.dispatch(updateTetri);
-        break;
+            event.preventDefault();
+            store.dispatch(updateTetri());
+            break;
+        case keyRight:
+            event.preventDefault();
+            store.dispatch(moveRight());
+            break;
+        case keyLeft:
+            event.preventDefault();
+            store.dispatch(moveLeft());
+            break;
+        case keyUp:
+            event.preventDefault();
+            store.dispatch(moveRot());
+            break;
     }
 }
