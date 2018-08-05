4b2fce41ac4b0d8d4b8677496b521dc346545772
Maxime LORIN <malorin@e1r5p13.42.fr>
2018-08-05 20:01:32 +0200

    suppression des anciens bouttons

--- src/client/containers/app.js
+++ src/client/containers/app.js
@@ -1,17 +1,9 @@
 import React from 'react';
-import UpdateTetri from '../components/UpdateTetri.js';
 import GameBoard from '../components/GameBoard.js';
-import MoveLeft from '../components/MoveLeft.js';
-import MoveRight from '../components/MoveRight.js';
-import MoveRot from '../components/MoveRot.js';
 
 
 const App = () => (
 	<div>
-		<UpdateTetri />
-		<MoveLeft />
-		<MoveRight />
-		<MoveRot />
 		<GameBoard />
 	</div>
 );
