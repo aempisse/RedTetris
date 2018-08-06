fe0bd677c1386b390c69b69c40bb16efd6f7b547
Axel EMPISSE <aempisse@student.42.fr>
2018-08-06 18:12:21 +0200

    turned server into a class

--- src/server/server.js
+++ src/server/server.js
@@ -1,5 +1,6 @@
-const server = require('http').createServer()
-const io = require('socket.io')(server)
+import express from 'express'
+import http from 'http'
+import IO from 'socket.io'
 
 const ClientManager = require('./ClientManager')
 const GameManager = require('./GameManager')
@@ -8,38 +9,51 @@ const makeHandlers = require ('./handlers')
 const clientManager = new ClientManager()
 const gameManager = new GameManager()
 
-io.on('connection', function (client) {
-    const {
-        handleRegister,
-        handleJoin,
-        handleLeave,
-        handleGetGames,
-        handleDisconnect
-    } = makeHandlers(client, clientManager, gameManager)
+class Server {
 
-    console.log('client connected...', client.id)
-    clientManager.addClient(client)
+    handeClient(socket) {
+        const {
+            handleRegister,
+            handleJoin,
+            handleLeave,
+            handleGetGames,
+            handleDisconnect
+        } = makeHandlers(client, clientManager, gameManager)
+    
+        console.log('client connected...', client.id)
+        clientManager.addClient(client)
+    
+        client.on('register', handleRegister)
+    
+        client.on('join', handleJoin)
+    
+        client.on('leave', handleLeave)
+    
+        client.on('games', handleGetGames)
+    
+        client.on('disconnect', function () {
+            console.log('client disconnect...', client.id)
+            handleDisconnect()
+        })
+    
+        client.on('error', function (err) {
+            console.log('received error from client:', client.id)
+            console.log(err)
+        })
+    }
 
-    client.on('register', handleRegister)
+    start() {
+        const app = express()
+        const server = http.Server(app)
+        const io = IO(server)
 
-    client.on('join', handleJoin)
+        io.on('connection', (socket) => this.handleClient(socket))
+        server.listen(3000, function (err) {
+            if (err) throw err
+            console.log('listening on port 3000')
+        })
+    }
 
-    client.on('leave', handleLeave)
+}
 
-    client.on('games', handleGetGames)
-
-    client.on('disconnect', function () {
-        console.log('client disconnect...', client.id)
-        handleDisconnect()
-    })
-
-    client.on('error', function (err) {
-        console.log('received error from client:', client.id)
-        console.log(err)
-    })
-})
-
-server.listen(3000, function (err) {
-    if (err) throw err
-    console.log('listening on port 3000')
-})
+const server = new Server().start()
