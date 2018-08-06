ffb5c35d5c5da19ffa7f430918b974745518410a
Axel EMPISSE <aempisse@student.42.fr>
2018-08-06 16:39:55 +0200

    first unit test

---
+++ test/Piece1.js
@@ -0,0 +1,40 @@
+import chai from "chai"
+import Piece from "../src/server/Piece"
+
+chai.should()
+
+describe('class Piece', function() {
+    let piece = new Piece()
+
+    it('should generate the same pattern for the same instance of Piece', function() {
+        let piece = new Piece()
+
+        let firstPattern = []
+        for (let i = 0; i < 20; i++) {
+            firstPattern[i] = piece.generatePiece(i);
+        }
+        let secondPattern = []
+        for (let i = 0; i < 20; i++) {
+            secondPattern[i] = piece.generatePiece(i);
+        }
+
+        firstPattern.toString().should.equal(secondPattern.toString())
+    })
+
+    it('should generate a different pattern for two instances of Piece', function() {
+        let piece1 = new Piece()
+        let piece2 = new Piece()
+
+        let patternPiece1 = []
+        for (let i = 0; i < 20; i++) {
+            patternPiece1[i] = piece1.generatePiece(i);
+        }
+
+        let patternPiece2 = []
+        for (let i = 0; i < 20; i++) {
+            patternPiece2[i] = piece2.generatePiece(i);
+        }
+
+        patternPiece1.should.not.equal(patternPiece2)
+    })
+})
