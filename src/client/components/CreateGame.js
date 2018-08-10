e78bb99e693ac446d59be2d10193d80670ac41b9
Maxime LORIN <malorin@e1r6p13.42.fr>
2018-08-10 16:51:14 +0200

    ajout du boutton create New Game

---
+++ src/client/components/CreateGame.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+const CreateGameButton = ({}) => {
+	return (
+		<div>
+            <button>Create New Game</button>
+		</div>
+	)
+}
+
+export default CreateGameButton;
